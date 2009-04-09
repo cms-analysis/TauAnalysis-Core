00b36e739467cd44db3287c43253e770148e9689
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2009-04-09 14:42:50 +0000

    changed input tags according to new PAT v2 workflow

--- python/metHistManager_cfi.py
+++ python/metHistManager_cfi.py
@@ -4,7 +4,7 @@ metHistManager = cms.PSet(
   name = cms.string('metHistManager'),
   type = cms.string('MEtHistManager'),
       
-  metSource = cms.InputTag('allLayer1METs'),
+  metSource = cms.InputTag('layer1METs'),
 
   dqmDirectory_store = cms.string('MEtQuantities')
 )

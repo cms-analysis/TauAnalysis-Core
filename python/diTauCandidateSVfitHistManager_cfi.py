a7b6afdf5354c790d41f7078704f0eafaee3550f
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2010-09-09 14:11:39 +0000

    added plots for polarized tau kinematics

--- python/diTauCandidateSVfitHistManager_cfi.py
+++ python/diTauCandidateSVfitHistManager_cfi.py
@@ -15,11 +15,15 @@ diTauCandidateSVfitHistManager = cms.PSet(
         cms.PSet(
             name = cms.string("psKine")
         ),
+        ##cms.PSet(
+        ##    name = cms.string("psKine_MEt")
+        ##),
+        ##cms.PSet(
+        ##    name = cms.string("psKine_MEt_ptBalance")
+        ##),
         cms.PSet(
-            name = cms.string("psKine_MEt")
-        ),
-        cms.PSet(
-            name = cms.string("psKine_MEt_ptBalance")
+            name = cms.string("polKine"),
+            polarizationHypotheses = cms.vstring("LL", "LR", "RL", "RR", "best{LL,RR}", "best{LR,RL}")
         )
     ),
 

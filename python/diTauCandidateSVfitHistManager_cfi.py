6ca8dcfa3cdf92c766a9987226e466b5c15f89bd
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2010-09-16 13:51:19 +0000

    re-enabled all polarization hypotheses

--- python/diTauCandidateSVfitHistManager_cfi.py
+++ python/diTauCandidateSVfitHistManager_cfi.py
@@ -15,15 +15,26 @@ diTauCandidateSVfitHistManager = cms.PSet(
         cms.PSet(
             name = cms.string("psKine")
         ),
-        ##cms.PSet(
-        ##    name = cms.string("psKine_MEt")
-        ##),
-        ##cms.PSet(
-        ##    name = cms.string("psKine_MEt_ptBalance")
-        ##),
+        cms.PSet(
+            name = cms.string("psKine_MEt")
+        ),
+        cms.PSet(
+            name = cms.string("psKine_MEt_ptBalance")
+        ),
         cms.PSet(
             name = cms.string("polKine"),
-            polarizationHypotheses = cms.vstring("LL", "LR", "RL", "RR", "best{LL,RR}", "best{LR,RL}", "best{LL,LR,RL,RR}")
+            polarizationHypotheses = cms.vstring("LL", "LR", "RL", "RR", "best{LL,RR}", "best{LR,RL}", "best{LL,LR,RL,RR}"),
+            massHypotheses = cms.vdouble(90.)
+        ),
+        cms.PSet(
+            name = cms.string("polKine_MEt"),
+            polarizationHypotheses = cms.vstring("LL", "LR", "RL", "RR", "best{LL,RR}", "best{LR,RL}", "best{LL,LR,RL,RR}"),
+            massHypotheses = cms.vdouble(90.)
+        ),
+        cms.PSet(
+            name = cms.string("polKine_MEt_ptBalance"),
+            polarizationHypotheses = cms.vstring("LL", "LR", "RL", "RR", "best{LL,RR}", "best{LR,RL}", "best{LL,LR,RL,RR}"),
+            massHypotheses = cms.vdouble(90.)
         )
     ),
 
